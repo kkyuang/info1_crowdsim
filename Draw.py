15b2c6ce733ff3141adaf462c141688c1583b67a
kkyuang <god@god.com>
2023-10-31 19:39:34 +0900

    .

---
+++ Draw.py
@@ -0,0 +1,41 @@
+import tkinter
+from Entity import Entity
+import numpy as np
+import time
+
+##윈도우 노출 코드
+window=tkinter.Tk()
+
+window.title("HELLO")
+window.geometry("640x400+100+100")
+window.resizable(False, False)
+
+##캔버스 정의
+canvas=tkinter.Canvas(window, width=640, height=480, bd=2)
+
+##원 그리기
+
+
+
+
+entitys = [Entity(np.array([3, 2]), 0.2, np.array([600, 90])), 
+           Entity(np.array([30, 50]), 0.2, np.array([600, 90])), 
+           Entity(np.array([30, 80]), 0.2, np.array([600, 90])), 
+           Entity(np.array([330, 50]), 0.2, np.array([600, 90])), 
+           Entity(np.array([320, 20]), 0.2, np.array([600, 90])), 
+           Entity(np.array([30, 90]), 0.2, np.array([600, 90]))]
+ 
+
+while(True):
+
+    canvas.delete("all")
+
+    for i in range(len(entitys)):
+        entitys[i].move()
+
+        arc=canvas.create_oval(entitys[i].position[0], entitys[i].position[1], entitys[i].position[0] + 30, entitys[i].position[1] + 30, fill='red')
+
+    canvas.pack()
+    window.update()
+    window.update_idletasks()
+    time.sleep(0.01)
---
+++ Entity.py
@@ -0,0 +1,22 @@
+import numpy as np
+import random
+
+class Entity:
+    def __init__(self, position, size, destination):
+        self.position = position.copy()
+        self.size = size
+
+        self.speed = 100
+        self.destination = destination
+
+        self.randomnoise = np.array([random.random(), random.random()])
+
+    def move(self):
+        #목적지를 향하는 방향 구하기
+        direction = self.destination - self.position + self.randomnoise * 50
+        direction = (direction) / np.linalg.norm(direction)
+
+        #목적지 방향으로 미소거리 더하기
+        self.position = self.position + direction * self.speed * 0.01
+    
+        
