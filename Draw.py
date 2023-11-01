9d71f01cb1a7266a4b73a7eed0382fdd37f83979
kkyuang <god@god.com>
2023-11-01 17:47:01 +0900

    .

--- Draw.py
+++ Draw.py
@@ -3,42 +3,55 @@ from Entity import Entity
 import numpy as np
 import time
 
-##윈도우 노출 코드
-window=tkinter.Tk()
 
-window.title("HELLO")
-window.geometry("640x400+100+100")
-window.resizable(False, False)
-
-##캔버스 정의
-canvas=tkinter.Canvas(window, width=640, height=480, bd=2)
-
-##원 그리기
-
-
-
-
-entitys = [Entity(np.array([3, 2]), 0.2, np.array([600, 90])), 
-           Entity(np.array([30, 50]), 0.2, np.array([600, 90])), 
-           Entity(np.array([30, 80]), 0.2, np.array([600, 90])), 
-           Entity(np.array([330, 50]), 0.2, np.array([600, 90])), 
-           Entity(np.array([320, 20]), 0.2, np.array([600, 90])), 
-           Entity(np.array([30, 90]), 0.2, np.array([600, 90]))]
-
-
+#GUI 화면 클래스(스크린사이즈, )
 class Drawer:
+    def __init__(self, screensize):
+        #윈도우 노출 코드
+        self.window=tkinter.Tk()
+
+        self.window.title("HELLO")
+        self.window.geometry(str(screensize[0]) + "x" + str(screensize[1]) + "+100+100")
+        self.window.resizable(False, False)
+
+        #캔버스 정의
+        self.canvas = tkinter.Canvas(self.window, width=screensize[0], height=screensize[1], bd=2)
+
+    def DrawCircle(self, center, radius):
+        self.canvas.create_oval(center[0] - radius/2, center[1] - radius/2,  center[0] + radius/2, center[1] + radius/2, fill='red')
+        self.canvas.pack()
+
+    def windowShow(self):
+        self.window.mainloop()
+
+    def canvasClear(self):
+        self.canvas.delete("all")
+
+    def windowUpdate(self):
+        self.window.update()
+        self.window.update_idletasks()
 
 
-while(True):
+dr = Drawer(np.array([640, 480]))        
 
-    canvas.delete("all")
+dt = 0
 
-    for i in range(len(entitys)):
-        entitys[i].move()
+startPosition = 0
+while 1:
+    #프레임 체크를 위한 시작 시간
+    startTime = time.time()
+    #화면 초기화
+    dr.canvasClear()
 
-        arc=canvas.create_oval(entitys[i].position[0], entitys[i].position[1], entitys[i].position[0] + 30, entitys[i].position[1] + 30, fill='red')
+    ##메인 프로그램 작성
+    startPosition += dt * 20
 
-    canvas.pack()
-    window.update()
-    window.update_idletasks()
-    time.sleep(0.01)
+    dr.DrawCircle(np.array([startPosition, 231]), 19)
+    dr.window.title(str(dt))
+
+    dr.windowUpdate()
+
+
+    #프레임 체크를 위한 루프의 마지막 시간
+    endTime = time.time()
+    dt = endTime - startTime
